e049bf4d61115e5fd4b4932fe8d0f840eb7df631
Sean Zehnder <sean@illcorporation.com>
2014-08-11 11:52:14 -0400

    fixed the calculator logic

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,81 +8,117 @@
 
 import UIKit
 
+enum OperatorType {
+    case Addition
+    case Subtraction
+    case Division
+    case Multiplication
+    case None
+    func name() -> String {
+        switch self {
+        case .Addition:
+            return "Addition"
+        case .Subtraction:
+            return "Subtraction"
+        case .Division:
+            return "Division"
+        case .Multiplication:
+            return "Multiplication"
+        case .None:
+            return "None"
+        }
+        
+    }
+    static func fromString(input:String) -> OperatorType {
+        switch input {
+            case "+":
+                return OperatorType.Addition
+            case "-":
+                return OperatorType.Subtraction
+            case "x":
+                return OperatorType.Multiplication
+            case "/":
+                return OperatorType.Division
+            default:
+                return OperatorType.None
+        }
+    }
+}
+
+enum InterfaceState {
+    case AwaitingFirstValue // empty
+    case AwaitingSecondValue // you've hit 5+
+    case PendingCalc // this would be when you hit 5*5+ (you have a pending multiplcation)
+    func name() -> String {
+        switch self {
+        case .AwaitingFirstValue:
+            return "AwaitingFirstValue"
+        case .AwaitingSecondValue:
+            return "AwaitingSecondValue"
+        case .PendingCalc:
+            return "PendingCalc"
+        }
+    }
+}
+
+
 class ViewController: UIViewController {
     
     var isNewNumberEntryInAnswerField = true
-    var numberOne:Double = 0
-    var numberTwo:Double = 0
-    var lastOperation:String = "Awaiting Second Number"
     
+    var currentValue:Double? = nil {
+        didSet {
+            if (self.currentValue == nil) {
+                answerLabel.text = "0"
+            } else {
+                answerLabel.text = toString(self.currentValue!)
+            }
+        }
+    }
+
+    var lastOperation:OperatorType?
+    var calculatorState:InterfaceState = .AwaitingFirstValue
+    var pendingOperation:OperatorType?
+    
+    var pendingValueInput:Double?
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    // MARK: IBAction Methods
     @IBAction func operationButtonDidPress(operationButton: UIButton) {
         isNewNumberEntryInAnswerField = true
         var operationType:String = operationButton.titleLabel.text
         var answer:Double = 0
-        println("the \(operationType) button was pressed")
-            switch lastOperation {
-                case "+":
-                    //the second number for the operation is in the answer label
-                    numberTwo = Double(answerLabel.text.toInt()!)
-                    //calculate the answer
-                    answer = numberOne + numberTwo
-                    //log it
-                    println("Performing the operation \(numberOne) plus \(numberTwo)")
-                    //put the answer in the screen
-                    answerLabel.text = toString(answer)
-                    //reset the number one answer
-                    numberOne = answer
-                    //record the current operation as the new last operation if it is not =
-                    if operationType != "=" {
-                        lastOperation = operationType
-                    }
-                case "-":
-                    println("Performing the operation \(numberOne) minus \(numberTwo)")
-                    numberTwo = Double(answerLabel.text.toInt()!)
-                    answer = numberOne - numberTwo
-                    answerLabel.text = toString(answer)
-                    numberOne = answer
-                    if operationType != "=" {
-                        lastOperation = operationType
-                    }
-                case "x":
-                    println("Performing the operation \(numberOne) times \(numberTwo)")
-                    numberTwo = Double(answerLabel.text.toInt()!)
-                    answer = numberOne * numberTwo
-                    answerLabel.text = toString(answer)
-                    numberOne = answer
-                    if operationType != "=" {
-                        lastOperation = operationType
-                    }
-                case "/":
-                    println("Performing the operation \(numberOne) divide by \(numberTwo)")
-                    numberTwo = Double(answerLabel.text.toInt()!)
-                    answer = numberOne / numberTwo
-                    answerLabel.text = toString(answer)
-                    numberOne = answer
-                    if operationType != "=" {
-                        lastOperation = operationType
-                    }
-                case "Awaiting Second Number":
-                    lastOperation = operationType
-                    numberOne = Double(answerLabel.text.toInt()!)
-                    println("awaiting second number")
-                default:
-                    println("there was an error")
-            }
+        
+        if (self.calculatorState == .AwaitingFirstValue) {
+            // next step is set the currentValue and perform no operation
+            currentValue = pendingValueInput
+            self.calculatorState = .AwaitingSecondValue
+            pendingValueInput = nil
         }
-    
-    
-
-    @IBAction func clearButton(sender: AnyObject) {
-        isNewNumberEntryInAnswerField = true
-        answerLabel.text = "0"
-        var numberOne:Double = 0
-        var numberTwo:Double = 0
-        var lastOperation:String = "Awaiting Second Number"
+        
+        self.performPendingOpIfExists()
+        
+        if (operationType != "=") {
+            self.pendingOperation = OperatorType.fromString(operationType)
+        }
     }
     
-    @IBOutlet weak var answerLabel: UILabel!
+    func performPendingOpIfExists() {
+        if (calculatorState == .AwaitingSecondValue && self.pendingOperation != OperatorType.None && self.pendingOperation != nil) {
+            println("pendingValueInput: \(pendingValueInput)")
+            self.currentValue = self.doCalculation(pendingValueInput!, operation: self.pendingOperation!)
+            self.pendingOperation = OperatorType.None
+        }
+    }
+ 
+    
+    @IBAction func clearButton(sender: AnyObject) {
+        isNewNumberEntryInAnswerField = true
+        currentValue = nil
+        lastOperation = nil
+        self.calculatorState = .AwaitingFirstValue
+    }
     
     @IBAction func numberTapped(myButton: UIButton) {
         if isNewNumberEntryInAnswerField == true {
@@ -93,9 +129,40 @@ class ViewController: UIViewController {
                 answerLabel.text = answerLabel.text + myButton.titleLabel.text
             }
         }
+
+        pendingValueInput = (answerLabel.text as NSString).doubleValue
     }
-   
     
+
+    
+    // MARK: Custom Methods
+    func doCalculation(second:Double?, operation:OperatorType) -> Double {
+
+        // NOTE: this what's often called a 'guard' clause
+        if (second == nil) {
+            return currentValue!
+        }
+        
+        var value = currentValue!
+        
+        switch operation {
+            case .Addition:
+                value = currentValue! + second!
+            case .Division:
+                value = currentValue! / second!
+            case .Multiplication:
+                value = currentValue! * second!
+            case .Subtraction:
+                value = currentValue! - second!
+            default:
+                println("do nothing")
+        }
+        
+        return value
+    }
+    
+    
+    // MARK: View Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
